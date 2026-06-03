e8a637ac4d3c9ccf7c21df15ba6d86ffc43dacdd
felipefariassilva78-ship-it <felipefariassilva78@gmail.com>
2026-06-03 08:35:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     <h1>Caras maneiros nunca traem</h1>
     <img src="https://i.pinimg.com/1200x/15/d2/54/15d25459b7bfa3564137569a73ef4b40.jpg">
     <input type="text">
+    <a href="https://souleater.fandom.com/pt-br/wiki/Soul_Eater_Wiki">Soul Eater Wiki</a>
     """
-<a href="https://souleater.fandom.com/pt-br/wiki/Soul_Eater_Wiki">Soul Eater Wiki</a>
 
 app.run(debug=True, host="0.0.0.0", port=10000)
