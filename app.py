251de1012a603ab266927b8b60391cd4d3c18856
felipefariassilva78-ship-it <felipefariassilva78@gmail.com>
2026-06-02 18:00:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Oxente painho</h1>"
+    return """
+    <h1>Caras maneiros nunca traem</h1>
+    <img src="https://i.pinimg.com/1200x/15/d2/54/15d25459b7bfa3564137569a73ef4b40.jpg">
+    """
 
 app.run(debug=True, host="0.0.0.0", port=10000)
