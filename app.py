d7eb2b7697cfe8e82bd9066a3abbca1fb1b86cbf
felipefariassilva78-ship-it <felipefariassilva78@gmail.com>
2026-06-02 18:18:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def home():
     return """
     <h1>Caras maneiros nunca traem</h1>
     <img src="https://i.pinimg.com/1200x/15/d2/54/15d25459b7bfa3564137569a73ef4b40.jpg">
+    <input type="text">
     """
 
 app.run(debug=True, host="0.0.0.0", port=10000)
