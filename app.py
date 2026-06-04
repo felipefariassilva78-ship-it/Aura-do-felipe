0912f333bcb76cdae7a652907b2aea9f0b1fdcb7
felipefariassilva78-ship-it <felipefariassilva78@gmail.com>
2026-06-03 19:37:18 -0700

    Refactor app.py for improved HTML structure

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def home():
     <p>Soul Eater (ソウルイーター, Sōru Ītā) conta a história de uma escola fictícia chamada Shibusen, localizada no estado americano de Nevada, criada para eliminar os ovos de Kishins, que são seres devoradores de almas humanas para se tornarem mais poderosos, e as Bruxas, que vivem aterrorizando o mundo. Os encarregados de eliminar esses seres são os artesãos, que além de eliminar os Kishins, devem ajudar suas armas a se tornarem Death Scythes, para o Shinigami usar como sua própria arma. Os artesãos devem fazer suas armas devorarem 99 ovos de Kishin e uma alma de bruxa para serem Death Scythes. O protagonista da série é Maka Albarn, uma jovem artesã que tem como arma o seu parceiro Soul Eater, um garoto que pode se transformar em uma foice. Juntos, eles enfrentam diversos desafios e inimigos enquanto buscam se tornar Death Scythes e proteger o mundo dos perigos que o ameaçam.</p>
     <a href="https://souleater.fandom.com/pt-br/wiki/Soul_Eater_Wiki">Soul Eater Wiki</a>
     <style>
+    p {
+    color: White;
+    }
         body { 
         background-color: gray;
         background-image: url('https://i.pinimg.com/736x/2a/ea/89/2aea89eabcab76bd190aab015e6ab66a.jpg');
@@ -24,6 +27,8 @@ def home():
         background-position: center;
         <p> {color: White; 
       }  h1 { color: White; }
+      h2 { color: White; }
+     
     </style>
  <button onclick="document.getElementById('musica').src += '&autoplay=1'">
     Abertura de Soul Eater
